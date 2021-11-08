d9fad9372162978bdf3f3bb4b9292fbac53aca30
QuentinLaruelle <71380161+QuentinLaruelle@users.noreply.github.com>
2021-11-08 15:25:55 +0100

    Update ChatBotModule.py

--- ChatBotModule.py
+++ ChatBotModule.py
@@ -36,13 +36,16 @@ def itineraire():
     lon1 = input("Entrez la longitude de la ville de départ")
     lat2 = input("Entrez la latitude de la ville d'arrivée")
     lon2 = input("Entrez la longitude de la ville d'arrivée")
+    # moyen de transport
     router = Router("car")
     start = router.findNode(lat1, lon1)
     finish = router.findNode(lat2, lon2)
     status, route = router.doRoute(start, finish)
     if status == 'success':
         pathLatLons = list(map(router.nodeLatLon, route))
+        #affichage des couples de coordonées jusqu'à la destination
         print(pathLatLons)
+
         
         
         
