47d393beca655055432a7a37b495a9ce9ee15665
QuentinLaruelle <71380161+QuentinLaruelle@users.noreply.github.com>
2021-11-22 00:31:06 +0100

    Update itineraire.py

--- Libs/itineraire.py
+++ Libs/itineraire.py
@@ -1,5 +1,23 @@
 # -*- coding: utf-8 -*-
 
+
+import openrouteservice
+
+coords = ((8.34234,48.23424),(8.34423,48.26424))
+
+client = openrouteservice.Client(key='5b3ce3597851110001cf624842459ea605184a62ac2aa7283c08ccbf') # Specify your personal API key
+routes = client.directions(coords)
+
+print(routes)
+
+
+
+
+
+
+
+
+"""
 from pyroutelib3 import Router
 
 
