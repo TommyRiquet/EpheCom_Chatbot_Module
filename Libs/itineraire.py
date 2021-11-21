cec25978ea6c286311a851d15139047684797616
QuentinLaruelle <71380161+QuentinLaruelle@users.noreply.github.com>
2021-11-22 00:36:29 +0100

    Update itineraire.py

--- Libs/itineraire.py
+++ Libs/itineraire.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-
-
 import openrouteservice
 
 coords = ((8.34234,48.23424),(8.34423,48.26424))
@@ -9,33 +7,3 @@ client = openrouteservice.Client(key='5b3ce3597851110001cf624842459ea605184a62ac
 routes = client.directions(coords)
 
 print(routes)
-
-
-
-
-
-
-
-
-"""
-from pyroutelib3 import Router
-
-
-def itineraire():
-    """
-    Fonction itinéraire qui
-    :return:
-    """
-    lat1 = input("Entrez la latitude de la ville de départ")
-    lon1 = input("Entrez la longitude de la ville de départ")
-    lat2 = input("Entrez la latitude de la ville d'arrivée")
-    lon2 = input("Entrez la longitude de la ville d'arrivée")
-    # moyen de transport
-    router = Router("car")
-    start = router.findNode(lat1, lon1)
-    finish = router.findNode(lat2, lon2)
-    status, route = router.doRoute(start, finish)
-    if status == 'success':
-        pathLatLons = list(map(router.nodeLatLon, route))
-        #affichage des couples de coordonées jusqu'à la destination
-        print(pathLatLons)
