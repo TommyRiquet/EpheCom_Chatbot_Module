b8457d6e249ef53b965663531e914053d253c278
QuentinLaruelle <71380161+QuentinLaruelle@users.noreply.github.com>
2021-11-22 02:29:08 +0100

    Update itineraire.py

--- Libs/itineraire.py
+++ Libs/itineraire.py
@@ -2,7 +2,7 @@
 
 import openrouteservice
 
-def itinéraire():
+def itineraire():
 coords = ((4.72628927230835,50.69315719604492),(4.6121757,50.6658724))#(long,lat)départ, (long,lat)arrivée
 
 client = openrouteservice.Client(key='5b3ce3597851110001cf624842459ea605184a62ac2aa7283c08ccbf') # Clef personnelle
