2fe7ddf531fa1e401eacbc0b6282f69e0c167bb2
Neow <71312500+Tommyriquet@users.noreply.github.com>
2021-11-24 17:26:54 +0100

    Update lien.py
    Ajout de liens  personnalisés
    Suppression des liens personnalisés

--- Libs/lien.py
+++ Libs/lien.py
@@ -1,14 +1,22 @@
 import webbrowser
 
-
-def lien():
-    site = input('Quel site souhaitez vous ouvrir? ')
-    if(site.lower() == "ephec"):
-        webbrowser.open("https://portail.ephec.be/")
-    elif(site.lower() == "tlca"):
-        webbrowser.open("https://www.tlca.eu/courses")
-    elif(site.lower() == "inginious"):
-        webbrowser.open("https://inginious.ephec.be/courselist")
+liens = {"ephec" : "https://portail.ephec.be/",
+         "inginious" : "https://inginious.ephec.be/",
+         "tlca" : "https://www.tlca.eu/"}
 
 
-lien()
+def add_lien(site,attr):
+    liens[site] = attr
+    print("Commande '"+site+"' ajouté avec succès")
+
+def rem_lien(site):
+    del liens[site]
+
+def lien(site):
+    try:
+        print(liens[site])
+    except KeyError:
+        pass
+
+
+
