8fb8706f8bffb04b0a9c6675fa2e91788d7518b6
Seb <he201808@students.ephec.be>
2021-12-12 22:22:12 +0100

    News resultats complet sans doublons

--- Libs/news.py
+++ Libs/news.py
@@ -1,36 +1,33 @@
 # -*- coding: utf-8 -*-
 
-from googlesearch import search             # Import Google
+from googlesearch import search             #import google
 from urllib.parse import urlparse
 
-
 class News:
-    """
-        Cette classe représente un module de News pour le Chatbot
-
-        Author : T. Riquet,S. Dziemianko
-        Date : December 2021
-    """
 
     def get_news(self, query, nbr_query=1):
         """
-        Cette methode recherche des résultats sur le web avec l'aide de la librairie Google
-
-        PRE : query est un str , nbr_query est un entier optionnel
-        POST : retourne les résultats de la cherche Google
-        RAISES : /
-
+        Recherche des résultats sur le web avec l'aide de la librairie Google
+        :param query: Le sujet de la recherche
+        :param nbr_query: Le nombre de recherche
+        :return:
         """
         link_increment = 0
         site = []
         response = ''
 
         # Recuperation des recherches
-        for url in search(str(query), tld='com', lang='fr', num=nbr_query, stop=nbr_query, pause=2):
+        for url in search(str(query), tld='com', lang='fr', num=10, stop=10, pause=3):
             domain = urlparse(url).netloc
             if domain not in site:
                 site.append(domain)
                 response += '\n' + url
                 link_increment += 1
 
-        return response
+            if link_increment == nbr_query:
+                return response
+
+
+        message = "Le temps alloué à la recherche à été excédé sans obtenir tout les resultats attendu. Possible que " \
+                  "vous avez demande trop de resultats ou qu'un nombre important de doublons se soit généré "
+        return response + '\n' + message
--- chatbot.py
+++ chatbot.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
-# Python 3.10
+# Python 3.9
 
 import Libs.command as command
 from Libs.meteo import Meteo
 from Libs.news import News
 from Libs.itineraire import Itineraire
+
+
+def send_message(reponse):
+    """
+    Renvoie la valeur de retour
+    """
+    print(reponse)
 
 
 class Chatbot:
@@ -16,40 +23,43 @@ class Chatbot:
     """
 
     def __init__(self):
-        """
-        PRE : /
-        POST : /
-        """
-        self.__list_command = ['help', 'meteo', 'news', 'itineraire', 'add', 'rem']
         self.__command = ''
         self.__attribut1 = ''
         self.__attribut2 = ''
         self.__attribut3 = ''
 
-    def call_module(self, com, message):
+    def get_command(self, message):
         """
-        Cette classe appelle les différents modules et renvoie la réponse
+        Cette méthode reçois les commandes du chat et va appeller les différents modules, en leurs passant les arguments
 
-        PRE : com est la commande utilisé par l'utilisateur , et message contient les arguments(optionnel)
-        POST : renvoie la réponse du module correspondant
+        PRE : message est un str
+        POST : retourne la réponse à la commande
         RAISES :
         -ValueError : lorsque l'ont introduit de mauvais arguments
         -IndexError : Lors des splits , si l'argument est manquant
         -KeyError : Lorsque les APIs utilisées ont atteint la limite journalière autorisée
+
         """
 
-        if com == 'help':
-            self.__attribut1 = message.split(' ')[1]
-            return command.get_help(self.__attribut1)
+        # HELP
+        if message.find('!help') == 0:
+            try:
+                self.__attribut1 = message.split(' ')[1]
+                return command.get_help(self.__attribut1)
+            except(ValueError, IndexError, KeyError):
+                return command.get_help()
 
-        elif com == 'meteo':
+        # METEO
+        if message.find('!meteo') == 0:
             try:
                 self.__attribut1 = message.split(' ')[1]
                 return Meteo().get_meteo(self.__attribut1)
             except (ValueError, IndexError, KeyError):
                 return "Ville manquante ou incorrecte (Ex: !meteo Paris)"
 
-        elif com == 'news':
+        # NEWS
+        elif message.find('!news') == 0:
+
             try:
                 self.__attribut1 = message[5:len(message) - 1]
                 if self.__attribut1 == " " or len(self.__attribut1) == 0:
@@ -69,7 +79,8 @@ class Chatbot:
                 else:
                     return News().get_news(self.__attribut1)
 
-        elif com == 'itineraire':
+        # ITINERAIRE
+        elif message.find("!itineraire") == 0:
             try:
                 self.__attribut1 = message.split("/")[0]
                 self.__attribut1 = self.__attribut1[12:]
@@ -83,47 +94,34 @@ class Chatbot:
             except (ValueError, IndexError, KeyError):
                 return "adresse incorecte (Ex:!itineraire Rue de l'example 123 / Rue de l'HTML 456 /route )"
 
-        elif com == 'add':
+        # ADD
+        elif message.find("!add") == 0:
             try:
                 self.__command = message.split(' ')[1]
                 self.__attribut1 = message[len(self.__command) + 6:]
-                response = command.add_lien(self.__command, self.__attribut1)
-                self.__list_command = command.get_command()
-                return response
+                return command.add_lien(self.__command, self.__attribut1)
             except IndexError:
                 return "Commande incorrecte => !add (nom de la commande) (ce qu'elle retourne)"
-        elif com == 'rem':
+
+        # REM
+        elif message.find("!rem") == 0:
             try:
                 self.__command = message.split(' ')[1]
                 return command.rem_lien(self.__command)
             except IndexError:
                 return "Commande incorrecte => !rem (nom de la commande)"
+
+        # OTHER COMMAND
         else:
             command.lien(message[1:])
-
-    def get_command(self, message):
-        """
-        Cette méthode reçois les commandes du chat et va appeller les différents modules, en leurs passant les arguments
-
-        PRE : message est un str
-        POST : retourne la réponse à la commande
-
-        """
-
-        if message.find('!') == 0:
-            # Recherche d'une commande dans le message
-            message = message[1:]
-            for coms in self.__list_command:
-                if message.find(coms) == 0:
-                    return self.call_module(coms, message)
-        else:
-            pass
 
 
 chatbot = Chatbot()
 
 message = '!'
 
-response = chatbot.get_command(message)
-if response is not None:
-    print(response)
+if message[0] == '!':
+    response = chatbot.get_command(message)
+    if response is not None:
+        send_message(response)
+        
