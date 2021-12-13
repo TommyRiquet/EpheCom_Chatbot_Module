32cd1a345c4f7978f6b9e065a41ed6d136f413cb
Seb <he201808@students.ephec.be>
2021-12-13 09:11:40 +0100

    News resultats complet sans doublons

--- Libs/news.py
+++ Libs/news.py
@@ -27,7 +27,4 @@ class News:
             if link_increment == nbr_query:
                 return response
 
-
-        message = "Le temps alloué à la recherche à été excédé sans obtenir tout les resultats attendu. Possible que " \
-                  "vous avez demande trop de resultats ou qu'un nombre important de doublons se soit généré "
-        return response + '\n' + message
+        return response
